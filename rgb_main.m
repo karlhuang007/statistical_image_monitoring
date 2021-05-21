df4f22fc71211fbdb10d7e5b9139f3428b0c0f65
huangxilin <karlhuang@qq.com>
2021-05-21 22:04:23 +0800

    change cov function

--- rgb_main.m
+++ rgb_main.m
@@ -127,39 +127,53 @@ end
 X_double_bar = mean(X_bar_set);
 [m,n] = size(X_bar_set);
 
-for i = 1:3
-    for j = 1:3
-        if i==j
-            cov(i,j) = var(X_bar_set(:,i));
-        else
-            num = 0.0;
-            for k = 1: m
-                num = num + ((X_bar_set(m,i) - X_double_bar(i)) * (X_bar_set(m,j) - X_double_bar(j)));
-            end
-            cov(i,j) = num / (m-1);
-        end
-    end
-end
-
+% for i = 1:3
+%     for j = 1:3
+%         if i==j
+%             covariance(i,j) = var(X_bar_set(:,i));
+%         else
+%             num = 0.0;
+%             for k = 1: m
+%                 num = num + ((X_bar_set(m,i) - X_double_bar(i)) * (X_bar_set(m,j) - X_double_bar(j)));
+%             end
+%             covariance(i,j) = 1 / (num-1);
+%         end
+%     end
+% end
+covariance = cov(X_bar_set);
+% for i = 1:3
+%     for j = 1:3
+%         if i==j
+%             cov(i,j) = var(X_bar_set(:,i));
+%         else
+% %             num = 0.0;
+% %             for k = 1: m
+% %                 num = num + ((X_bar_set(m,i) - X_double_bar(i)) * (X_bar_set(m,j) - X_double_bar(j)));
+% %             end
+%             cov(i,j) = cov(X_bar_set(:,i).',X_bar_set(:,j).');
+%         end
+%     end
+% end
+% cov_ = cov()
 % autocorelate plot of six parameter
-subplot(3,1,1);
-autocorr(X_bar_set(:,1));
-title('autocorr plot of the maximal value of diaganal detail coefficient in Red frame of phase I images');
-subplot(3,1,2);
-autocorr(X_bar_set(:,2));
-title('autocorr plot of the maximal value of diaganal detail coefficient in Green frame of phase I images');
-
-subplot(3,1,3);
-autocorr(X_bar_set(:,3));
-title('autocorr plot of the maximal value of diaganal detail coefficient in Blue frame of phase I images');
-
+% subplot(3,1,1);
+% autocorr(X_bar_set(:,1));
+% title('autocorr plot of the maximal value of diaganal detail coefficient in Red frame of phase I images');
+% subplot(3,1,2);
+% autocorr(X_bar_set(:,2));
+% title('autocorr plot of the maximal value of diaganal detail coefficient in Green frame of phase I images');
+% 
+% subplot(3,1,3);
+% autocorr(X_bar_set(:,3));
+% title('autocorr plot of the maximal value of diaganal detail coefficient in Blue frame of phase I images');
+% 
 
 % comparative test(normal image) 
 test_result = [];
 for i=41:54
     [r,g,b]=haar2_wavelet(sprintf('image_part_0%d.jpg',i));
     X_bar = [r,g,b];
-    T2 =  ([X_bar - X_double_bar] * inv(cov) *[X_bar - X_double_bar].' );
+    T2 =  ([X_bar - X_double_bar] * inv(covariance) *[X_bar - X_double_bar].' );
    test_result = [test_result,T2];
 end
 % comparative test(defect image) 
@@ -167,7 +181,7 @@ defect_result = [];
 for i=1:5
     [r,g,b]=haar2_wavelet(sprintf('b%d.png',i));
     X_bar = [r,g,b];
-    T2 =  ([X_bar - X_double_bar] * inv(cov) *[X_bar - X_double_bar].' );
+    T2 =  ([X_bar - X_double_bar] * inv(covariance) *[X_bar - X_double_bar].' );
    defect_result = [defect_result,T2];
 end
 
