8d1031616e87168689bfe2d03cade8da56e05cdc
huangxilin <karlhuang@qq.com>
2021-06-30 10:58:02 +0800

    add light scratch image in hotelling t2 method

--- phase_II/PHASEII/test_main.m
+++ phase_II/PHASEII/test_main.m
@@ -4,20 +4,35 @@ load cov_and_x_bar_100.mat;
 imagefiles = dir('*.jpg');      
 nfiles = length(imagefiles);    % Number of files found
 test_result = [];
-for ii=1:nfiles
-   currentfilename = imagefiles(ii).name;
-%    currentimage = imread(currentfilename);
-   [r,g,b]=haar2_wavelet(currentfilename);
-   X_bar = [r,g,b];
+%% for image_part_00x
+% for ii=1:nfiles
+%    currentfilename = imagefiles(ii).name;
+% %    currentimage = imread(currentfilename);
+%    [r,g,b]=haar2_wavelet(currentfilename);
+%    X_bar = [r,g,b];
+%     T2 =  ([X_bar - X_double_bar] * inv(covariance) *[X_bar - X_double_bar].' );
+%    test_result = [test_result,T2];
+%    
+% end
+% x=[1:1:121];
+% x1 = [1:1:121];
+% x2 = 8.276*ones([1,121]);
+% 
+% plot(x,test_result,x1,x2);
+% title("Phase II image statistic(orange line is UCL = 8.276)")
+% xlabel("image number")
+% ylabel("T2 statistic")
+%% for bx.jpg
+for i=1:15
+    
+    [r,g,b]=haar2_wavelet(sprintf('b%d.jpg',i));
+    X_bar = [r,g,b];
     T2 =  ([X_bar - X_double_bar] * inv(covariance) *[X_bar - X_double_bar].' );
-   test_result = [test_result,T2];
-   
+    test_result = [test_result,T2];
 end
-
-x=[1:1:121];
-x1 = [1:1:121];
-x2 = 8.276*ones([1,121]);
-
+x=[1:1:15];
+x1 = [1:1:15];
+x2 = 8.276*ones([1,15]);
 plot(x,test_result,x1,x2);
 title("Phase II image statistic(orange line is UCL = 8.276)")
 xlabel("image number")
